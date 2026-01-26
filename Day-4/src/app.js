91ac6e6c9c9a5ed6965bcdfbad73fb18fc287792
Aman Jaiswal <amanjaiswal262001@gmail.com>
2026-01-26 17:21:41 +0530

    day-4 completed

--- Day-4/src/app.js
+++ Day-4/src/app.js
@@ -1,12 +1,11 @@
 const express = require("express");
-
 const app = express();// server yaha start ho gya
 
 app.use(express.json())
 const notes = [];
 
 //POST  /notes
-app.post("/notes", (req, res) =>{
+app.post("/notes", (req, res) => {
     console.log(req.body);
     notes.push(req.body)
 
@@ -14,26 +13,22 @@ app.post("/notes", (req, res) =>{
     res.send("notes created ");
 })
 
-
-
 // GET  /notes
-app.get("/notes", (req, res)=> {
+app.get("/notes", (req, res) => {
     res.send(notes)
-}) 
+})
 
 // DELETE /notes karne wali api create
 // params use karna hoga yaha
-app.delete("/notes/:index", (req, res)=>{
-
+app.delete("/notes/:index", (req, res) => {
     delete notes[req.params.index]
-    
     res.send("note deleted successfully")
 })
 
 //PATCH /notes/:index
 // discription update karna hai bas 
 
-app.patch("/notes/:index", (req,res) => {
+app.patch("/notes/:index", (req, res) => {
     console.log()
     notes[req.params.index].description = req.body.description;
     res.send("discription updated successfully")
