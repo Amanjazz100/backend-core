467475f2b8ca0aee1ea952370ae69ddbcc15a7c0
Aman Jaiswal <amanjaiswal262001@gmail.com>
2026-03-19 00:00:22 +0530

    Add initial setup for React application with Vite
    - Created main entry point (main.jsx) for the React app.
    - Added global styles in index.css for consistent layout and design.
    - Included React and Vite configuration files (vite.config.js).
    - Added SVG assets for React and Vite logos.

--- day-9/Backend/src/app.js
+++ day-9/Backend/src/app.js
@@ -1,9 +1,11 @@
 
 const express = require('express');
 const noteModel = require('./models/note.model')
+const cors = require('cors')
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 //api banayege yaha
---
+++ day-9/Frontend/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import './index.css'
+import axios from 'axios'
+
+function App() {
+
+  const [notes, setnotes] = useState([
+    {
+      title: "test title",
+      description: "test description"
+    },
+    {
+      title: "test title 2",
+      description: "test description 2"
+    },
+    {
+      title: "test title 3",
+      description: "test description 3"
+    },
+    {
+      title: "test title 4",
+      description: "test description 4"
+    },
+  ])
+
+  axios.get('http://localhost:3000/api/notes')
+  .then((res)=>{
+    setnotes(res.data.notes) 
+  })
+
+  return (
+   <>
+    <div className="notes">
+      {
+        notes.map(notes => {
+          return (
+            <div className="note">
+              <h1>{notes.title}</h1>
+              <p>{notes.description}</p>
+            </div>
+          )
+        })
+      }
+    </div>
+   </> 
+  ) 
+}
+
+export default App
